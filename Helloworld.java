0366ff07952b86c5b25134761fe3398b57998d8f
bhaskar611 <77197575+bhaskar611@users.noreply.github.com>
2021-01-31 20:09:24 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,8 @@ class HelloWorld {
             public static void main(String args[]){
                 System.out.println("HelloWorld");
                 System.out.println("New");
-                System.out.println("NewFeature");
+                System.out.println("NewFeature
+                System.out.println("haii");
 }
 }
 

84fbd66a502503e375100098504605f34bd0aac0
bhaskar611 <77197575+bhaskar611@users.noreply.github.com>
2021-01-31 20:15:03 +0530

    Create Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ class HelloWorld {
             public static void main(String args[]){
                 System.out.println("HelloWorld");
                 System.out.println("New");
-                System.out.println("NewFeature
+                System.out.println("NewFeature);
                 System.out.println("haii");
 }
 }
